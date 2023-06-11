3c42709fa3b737770ee271969556817105c70a89
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-11 10:27:33 +0300

    Create Program.cs

---
+++ 01_HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Cs_language
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+        Console.WriteLine("Hello World!.");
+        }
+    }
+}
