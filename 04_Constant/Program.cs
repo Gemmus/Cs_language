9592bc8fd26a6db6296cf568ccca21ea18287e50
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-12 12:05:26 +0300

    Create Program.cs

---
+++ 04_Constant/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Cs_language
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+            Console.WriteLine(pi);
+
+            Console.ReadKey();
+        }
+    }
+}
