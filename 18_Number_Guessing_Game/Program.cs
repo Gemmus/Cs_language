e005b8694e3802fc2166de9c197710026f0d2c55
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-14 10:11:51 +0300

    Create Program.cs

---
+++ 18_Number_Guessing_Game/Program.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Cs_language
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            int guesses;
+            String response;
+
+            while(playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                response = "";
+                number = random.Next(min, max + 1);
+
+                while(guess != number)
+                {
+                    Console.WriteLine("Guess a number between " + min + " - " + max + ": ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+
+                    if (guess > number) Console.WriteLine(guess + " is too high.");
+                    else if (guess < number) Console.WriteLine(guess + " is too low.");
+                    guesses++;
+                }
+                Console.WriteLine("The number is: " + number);
+                Console.WriteLine("You won by guessing " + guesses + " times!");
+
+                Console.WriteLine("Would you like to play again (Y/N)? ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y") playAgain = true;
+                else playAgain = false;
+
+                Console.WriteLine();
+            } 
+
+            Console.WriteLine("Thanks for playing!");
+            Console.WriteLine("Bye!");
+
+            Console.ReadKey();
+        }
+    }
+}
